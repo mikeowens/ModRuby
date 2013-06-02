288376d1359eba9a6fec685093ba947fe69f5353
Mike <mikeowens@gmail.com>
2013-06-01 22:31:18 -0500

    Fixed HTTP auth username method in Request class

--- src/ruby_request.cpp
+++ src/ruby_request.cpp
@@ -239,7 +239,7 @@ void init_request(VALUE module)
     rb_define_method(cls, "the_request", (fn)m_the_request, 0);
     rb_define_method(cls, "unparsed_uri", (fn)m_unparsed_uri, 0);
     rb_define_method(cls, "uri", (fn)m_uri, 0);
-    rb_define_method(cls, "m_user", (fn)m_user, 0);
+    rb_define_method(cls, "user", (fn)m_user, 0);
     rb_define_method(cls, "vlist_validator", (fn)m_vlist_validator, 0);
     rb_define_method(cls, "write", (fn)m_write, 2);
 }
