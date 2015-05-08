25f7eae939446d849757384fd80f7af335fc551e
Mike Owens <michael.owens@linterra.org>
2015-05-08 11:35:38 -0500

    Default external set to UTF-8

--- config/linux/debian/settings.cmake
+++ config/linux/debian/settings.cmake
@@ -13,17 +13,17 @@ SET(HTTPD "/usr/sbin/apache2")
 
 # Parameters for locating Ruby
 SET(RUBY_POSSIBLE_INCLUDE_PATHS
-  /usr/include/ruby-2.1.0/
-  /usr/include/ruby-2.1.0/${CMAKE_SYSTEM_PROCESSOR}-linux/
+  /usr/include/ruby-2.2.0/
+  /usr/include/ruby-2.2.0/${CMAKE_SYSTEM_PROCESSOR}-linux/
   /usr/include/ruby-2.0.0/
   /usr/include/ruby-2.0.0/${CMAKE_SYSTEM_PROCESSOR}-linux/ )
 
 SET(RUBY_POSSIBLE_INCLUDE_ARCH_PATHS 
-  /usr/include/ruby-2.1.0/
-  /usr/include/ruby-2.1.0/${CMAKE_SYSTEM_PROCESSOR}-linux/
-  /usr/include/ruby-2.1.0/i486-linux/
-  /usr/include/ruby-2.1.0/i586-linux/
-  /usr/include/ruby-2.1.0/i686-linux/ 
+  /usr/include/ruby-2.2.0/
+  /usr/include/ruby-2.2.0/${CMAKE_SYSTEM_PROCESSOR}-linux/
+  /usr/include/ruby-2.2.0/i486-linux/
+  /usr/include/ruby-2.2.0/i586-linux/
+  /usr/include/ruby-2.2.0/i686-linux/ 
   /usr/include/ruby-2.0.0/
   /usr/include/ruby-2.0.0/${CMAKE_SYSTEM_PROCESSOR}-linux/
   /usr/include/ruby-2.0.0/i486-linux/
