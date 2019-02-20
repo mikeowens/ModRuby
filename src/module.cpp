5507f56cfd38c5ae3587c5249948de60e602f5b9
Cal Heldenbrand <cal@fbsdata.com>
2019-02-20 13:38:18 -0600

    On exception, return DONE instead of OK
    We might need to revert this as I'm unsure of the side effects
    this might have on functionality.  Returning DONE will cause
    the server to close this thread without further processing.
    
    Returning OK will proceed on to other modules, which in the case of
    redirects, termination, and other errors seems like a bad idea.

--- src/module.cpp
+++ src/module.cpp
@@ -574,7 +574,7 @@ int ruby_request_handler(request_rec* r)
         if (ruby_request_init_configuration(r) != 0)
         {
             // Configuration failed
-            return OK;
+            return DONE;
         }
 
         modruby::Handler handler = ruby_request_get_handler(r);
@@ -589,9 +589,8 @@ int ruby_request_handler(request_rec* r)
                           "mod_ruby[%i] : %s",
                           getpid(), "Failed to load handler" );
 
-            return OK;
+            return DONE;
         }
-
         // Call the method, passing in the request object
         VALUE result = handler.object->method( handler.method(), 1,
                                                make_request(r) );
@@ -614,7 +613,7 @@ int ruby_request_handler(request_rec* r)
             // This is a utility exception used within the framework. It does
             // not indicate an error.
 
-            return OK;
+            return DONE;
         }
 
         if (exception_type == "ModRuby::RequestTermination")
@@ -622,7 +621,7 @@ int ruby_request_handler(request_rec* r)
             // This is a utility exception used within the framework. It does
             // not indicate an error.
 
-            return OK;
+            return DONE;
         }
 
         // Else it's a bonified exception. Get the message and stacktrace.
@@ -644,7 +643,7 @@ int ruby_request_handler(request_rec* r)
         // HTTP OK. By convention, the HTTP response status is not what matters
         // here, it's what's in the headers.
 
-        return OK;
+        return DONE;
     }
     catch (const std::exception& e)
     {
@@ -659,7 +658,7 @@ int ruby_request_handler(request_rec* r)
                       "mod_ruby[%i] : %s",
                       getpid(), e.what() );
 
-        return OK;
+        return DONE;
     }
 
     // If the status changed, use its value
@@ -726,7 +725,7 @@ int ruby_generic_handler( request_rec* r,
             // Set the TestResult Header
             request.headers_out().set("TestResult", "PASS");
 
-            return OK;
+            return DONE;
         }
 
         if (exception_type == "ModRuby::RequestTermination")
@@ -737,7 +736,7 @@ int ruby_generic_handler( request_rec* r,
             // Set the TestResult Header
             request.headers_out().set("TestResult", "PASS");
 
-            return OK;
+            return DONE;
         }
 
         // Else it's a bonified exception. Get the message and stacktrace.
@@ -758,7 +757,7 @@ int ruby_generic_handler( request_rec* r,
         // HTTP OK. By convention, the HTTP response status is not what matters
         // here, it's what's in the headers.
 
-        return OK;
+        return DONE;
     }
     catch (const std::exception& e)
     {
@@ -772,7 +771,7 @@ int ruby_generic_handler( request_rec* r,
         ap_log_error( APLOG_MARK, APLOG_CRIT, 0, r->server,
                       "mod_ruby[%i] : %s", getpid(), e.what() );
 
-        return OK;
+        return DONE;
     }
 
     // If the status changed, use its value
@@ -796,3 +795,4 @@ int ruby_request_script_handler(request_rec* r)
 {
     return ruby_generic_handler(r, "ruby-script-handler", "script");
 }
+
